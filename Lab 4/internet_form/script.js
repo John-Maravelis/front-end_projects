a891195c89102239a8739688942f76a013e0d01b
John-Maravelis <47307883+John-Maravelis@users.noreply.github.com>
2021-01-06 21:33:46 +0200

    Fixed the main bug

--- Lab 4/internet_form/script.js
+++ Lab 4/internet_form/script.js
@@ -7,40 +7,41 @@
 // var secondMask = 255;
 // var thirdMask = 255;
 // var fourthMask = 0;
- 
-window.addEventListener("load", () => {
-	var firstIP = document.getElementById("firstIP");
-	var secondIP = document.getElementById("secondIP");
-	var thirdIP = document.getElementById("thirdIP");
-	var fourthIP = document.getElementById("fourthIP");
 
-	var firstMask = document.getElementById("firstMask");
-	var secondMask = document.getElementById("secondMask");
-	var thirdMask = document.getElementById("thirdMask");
-	var fourthMask = document.getElementById("fourthMask");
+var firstIP = document.getElementById("firstIP");
+var secondIP = document.getElementById("secondIP");
+var thirdIP = document.getElementById("thirdIP");
+var fourthIP = document.getElementById("fourthIP");
 
-	var form = document.getElementById("form");
+var firstMask = document.getElementById("firstMask");
+var secondMask = document.getElementById("secondMask");
+var thirdMask = document.getElementById("thirdMask");
+var fourthMask = document.getElementById("fourthMask");
 
-	form.addEventListener("submit", (e) => {
-		let alertMessage;
-		let flag = false; 
-		if (firstIP.value > 255 || secondIP.value > 255 || thirdIP.value > 255 || fourthIP.value > 255) {
-			flag = true;
-			alertMessage = "Incorrect values";
-		}
+var form = document.getElementById("form");
+
+// form.addEventListener("submit", (e) => {
+	function init(){
+	let alertMessage;
+	let flag = false;
+	if (firstIP.value > 255 || secondIP.value > 255 || thirdIP.value > 255 || fourthIP.value > 255) {
+		flag = true;
+		alertMessage = "Incorrect values";
+	}
+
+	if (firstMask.value > 255 || secondMask.value > 255 || thirdMask.value > 255 || fourthMask.value > 255) {
+		flag = true;
+		alertMessage = "Incorrect values";
+	}
+
+	if (flag === true) {
+		alert(alertMessage);
+		// e.preventDefault();
+	}
 	
-		if (firstMask.value > 255 || secondMask.value > 255 || thirdMask.value > 255 || fourthMask.value > 255) {
-			flag = true;
-			alertMessage = "Incorrect values";
-		}
-	
-		if (flag === true) {
-			alert(alertMessage);
-			e.preventDefault();
-		}
-	});
-}, false); 
-
+getResults();	
+}
+// });
 
 function convertToBin(num) {
 	return Number(num).toString(2);
